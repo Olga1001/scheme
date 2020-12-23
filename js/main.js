dcbbbf38475c805a9dc2ff713b454bf313242afa
Olga <zhuravel.olga95@gmail.com>
2020-12-23 16:25:51 +0200

    fixed hover over pointer for mobile

--- js/main.js
+++ js/main.js
@@ -1,39 +1,36 @@
 $(document).ready(function () {
+  var $pz = $("#panzoom");
 
-  if (window.matchMedia("(min-width: 1025px)").matches) {
-    $("#panzoom").panzoom({
-      $zoomIn: $(".zoom-in"),
-      $zoomOut: $(".zoom-out"),
-      $zoomRange: $(".zoom-range"),
-      minScale: 1,
-      maxScale: 2,
-      // increment: 4,
-      // pan: true,
-      contain: 'invert',
-      disablePan: false,
-      disableZoom: false
-    });
-  } else {
+  $pz.panzoom({
+    $zoomIn: $(".zoom-in"),
+    $zoomOut: $(".zoom-out"),
+    $zoomRange: $(".zoom-range"),
+    minScale: 1,
+    maxScale: 2,
+    // increment: 4,
+    // pan: true,
+    contain: 'invert',
+    pan: true,
+    zoom: true,
+    // ...or set a class on the element
+    excludeClass: 'pin',
+    disableYAxis: true,
+    disableXAxis: true,
+  });
+
   var zm = new Zoom(document.getElementById('panzoom'), {
     pan: false,
     rotate: false,
+    excludeClass: 'pin'
   });
-  }
-
-
 
   $(".pin").hover(function () {
     let iPin = $(this).attr("data-index");
     $('[data-index="'+ iPin +'"]').toggleClass('active');
   });
+  $(".pin").bind('touchstart touchend', function (){
+    let iPin = $(this).attr("data-index");
+    $('[data-index="'+ iPin +'"]').toggleClass('active');
+  });
 
-  // $(".pin").on("mouseover", function () {
-  //   let iPin = $(this).attr("data-index");
-  //   $('[data-index="'+ iPin +'"]').addClass('active');
-  // });
-
-  // $(".pin").on("mouseout", function () {
-  //   let iPin = $(this).attr("data-index");
-  //   $('[data-index="'+ iPin +'"]').removeClass('active');
-  // });
 });
