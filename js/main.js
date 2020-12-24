8338b915aded7f1c399ee407cd6aee82181ec369
Olga <zhuravel.olga95@gmail.com>
2020-12-24 19:28:43 +0200

    fixed drug on desktop and touch focus for input

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 $(document).ready(function () {
-  var $pz = $("#panzoom");
 
-  $pz.panzoom({
-    $zoomIn: $(".zoom-in"),
-    $zoomOut: $(".zoom-out"),
-    $zoomRange: $(".zoom-range"),
-    minScale: 1,
-    maxScale: 2,
-    // increment: 4,
-    // pan: true,
-    contain: 'invert',
-    pan: true,
-    zoom: true,
-    // ...or set a class on the element
-    excludeClass: 'pin',
-    disableYAxis: true,
-    disableXAxis: true,
-  });
+  if (window.matchMedia("(min-width: 1025px)").matches) {
+    var $pz = $("#panzoom");
 
-  var zm = new Zoom(document.getElementById('panzoom'), {
-    pan: false,
-    rotate: false,
-    excludeClass: 'pin'
-  });
+    $pz.panzoom({
+      $zoomIn: $(".zoom-in"),
+      $zoomOut: $(".zoom-out"),
+      $zoomRange: $(".zoom-range"),
+      minScale: 1,
+      maxScale: 2,
+      // increment: 4,
+      // pan: true,
+      contain: 'invert',
+      pan: true,
+      zoom: true,
+      // ...or set a class on the element
+      excludeClass: 'pin',
+      disableYAxis: true,
+      disableXAxis: true,
+    });
+  } else {
+    var zm = new Zoom(document.getElementById('panzoom'), {
+      pan: false,
+      rotate: false,
+      excludeClass: 'pin'
+    });
+  }
+
 
   $(".pin").hover(function () {
     let iPin = $(this).attr("data-index");
@@ -32,7 +36,7 @@ $(document).ready(function () {
     let iPin = $(this).attr("data-index");
     $('[data-index="'+ iPin +'"]').toggleClass('active');
   });
-  $(".modal input").on("click touchstart", function() {
+  $(".modal_count input").on("click touchstart touchend", function() {
     $(this).focus();
   });
  
