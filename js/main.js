ccebe463e98814c44b8075f3baeaf7785a9cc701
Olga <zhuravel.olga95@gmail.com>
2020-12-28 12:46:16 +0200

    updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
   if (window.matchMedia("(min-width: 1025px)").matches) {
     var $pz = $("#panzoom");
 
@@ -12,21 +13,22 @@ $(document).ready(function () {
       // increment: 4,
       // pan: true,
       contain: 'invert',
-      pan: true,
-      zoom: true,
+      // pan: true,
+      // zoom: true,
       // ...or set a class on the element
-      excludeClass: 'pin',
-      disableYAxis: true,
-      disableXAxis: true,
+      // excludeClass: 'pin',
+      // disableYAxis: true,
+      // disableXAxis: true,
     });
+  
   } else {
     var zm = new Zoom(document.getElementById('panzoom'), {
       pan: false,
       rotate: false,
       excludeClass: 'pin'
     });
+   
   }
-
 
   $(".pin").hover(function () {
     let iPin = $(this).attr("data-index");
@@ -36,8 +38,14 @@ $(document).ready(function () {
     let iPin = $(this).attr("data-index");
     $('[data-index="'+ iPin +'"]').toggleClass('active');
   });
-  $(".modal_count input").on("click touchstart touchend", function() {
+
+  $(".modal_count input").on("click", function() {
     $(this).focus();
   });
- 
+
+  $(".modal_count input").on("touchstart touchend", function(e) {
+    e.stopPropagation();
+    // $(this).focus();
+  });
+  
 });
